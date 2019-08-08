818808fae285b86ad6bb008ce8a0372e01c93d49
1jyotsna1 <42210760+1jyotsna1@users.noreply.github.com>
2019-08-08 22:00:07 +0530

    Rename ArdSensor.ino to Arduino/ArdSensor.ino

---
+++ Arduino/ArdSensor.ino
@@ -0,0 +1,24 @@
+int sensor = A0;
+void setup()
+
+{
+   
+
+ 
+  pinMode(sensor,INPUT);
+ 
+  Serial.begin(9600);
+
+ 
+}
+
+void loop()
+{
+
+
+int sensor_value = analogRead(sensor);
+
+
+  Serial.println(sensor_value);
+
+}
